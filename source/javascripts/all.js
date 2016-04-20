d272ebc7531e53cf92c953ac2b983ddb2e80daa8
Varsatorul <mihai14x7@gmail.com>
2016-04-20 11:55:58 +0300

    Hello Button now communicates with the Pushbullet API

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,17 @@
-// This is where it all goes :)
+// Hello button code
+var HelloModule = (function () {
+  var triggerPush = function () {
+    $.get('http://localhost:3000/hello'); // TODO: Change before deploying
+  };
+
+  return {
+    updateButton: function () {
+      $('div#hello-button').children('p.send').fadeOut(300);
+      $('div#hello-button').children('p.sent').fadeIn(600);
+      triggerPush();
+    }
+  }
+})();
+
+// Hello Button Event Handler
+$('div#hello-button').click(HelloModule.updateButton);
